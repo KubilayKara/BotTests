ca332fe2c4f05796f39309b1617cd6baab9ca100
Kubilay <karakubilay@hotmail.com>
2017-04-24 11:03:13 +0300

    luis test

---
+++ Bot ApplicationTest/Dialogs/LuisTestDialog.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Bot_ApplicationTest.Dialogs
+{
+    [LuisModel("1de929c1-b424-46a4-a644-edd3323b39a7", "7ad9e83d30154d0aa6061ff8ba0ba4bb")]
+    [Serializable]
+    public class LuisTestDialog:LuisDialog<object>
+    {
+
+        [LuisIntent("Greetings")]
+        public async Task Greeting(IDialogContext context, LuisResult result)
+        {
+            string message = "Wellcome";
+            await context.PostAsync(message);
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Sorry I did not understand: " + string.Join(", ", result.Intents.Select(i => i.Intent));
+            await context.PostAsync(message);
+            context.Wait(MessageReceived);
+        }
+    }
+}

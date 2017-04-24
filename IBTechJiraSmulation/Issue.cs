5e8dc3095abc2e9d6a4981ce63e85e203181717f
Kubilay <karakubilay@hotmail.com>
2017-04-24 20:34:30 +0300

    jira simulation

---
+++ IBTechJiraSmulation/Issue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBTechJiraSmulation
+{
+    public class Issue
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Deadline { get; set; }
+        public string AssigneeCode { get; set; }
+        public IssueStatus Status { get; set; }
+
+    }
+
+    public  enum IssueStatus
+    {
+        None,
+        Open,
+        Inprogress,
+        Closed
+    }
+}
